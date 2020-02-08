36cbba0477c81ec69963bf706bd1454618e18d03
William Roger <williamroger.frontend@gmail.com>
2020-02-08 19:55:53 -0300

    Adicionado alguns comentários

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ app.use(express.json());
 const projects = [];
 let count = 0;
 
-// Middlewares
+/**
+ * Middleware que exibe um log do número de requisições
+ */
 app.use((request, reponse, next) => {
   count++;
-  
+
   if (count > 1) {
     console.log(`Já foram realizadas ${count} requisições`);
   } else {
@@ -19,6 +21,9 @@ app.use((request, reponse, next) => {
   next();
 });
 
+/**
+ * Middleware que checa se o projeto existe
+ */
 function checkProjectExists(request, response, next) {
   const { id } = request.params;
   const projectExists = projects.some((project) => {
@@ -26,13 +31,15 @@ function checkProjectExists(request, response, next) {
   });
   
   if (!projectExists) {
-    return response.status(400).json({"message": "Projeto não encontrado."});
+    return response.status(400).json({message: "Projeto não encontrado."});
   }
 
   return next();
 }
 
-// Helper function
+/**
+ * Função que retorna o index de um projeto no array projects
+ */
 function getIndexByProject(id) {
   let index;
 
@@ -45,6 +52,10 @@ function getIndexByProject(id) {
   return index;
 }
 
+/**
+ * Request body: id, title
+ * Cadastra um novo projeto
+ */
 app.post('/projects', (request, response) => {
   const { id, title } = request.body;
 
@@ -53,10 +64,18 @@ app.post('/projects', (request, response) => {
   return response.json(projects);
 });
 
+/**
+ * Retorna todos os projetos cadastrados
+ */
 app.get('/projects', (request, response) => {
   return response.json(projects);
 });
 
+/**
+ * Route params: id
+ * Request body: title
+ * Altera o título do projeto com o id informado no parâmetro da rota
+ */
 app.put('/projects/:id', checkProjectExists, (request, response) => {
   const { id } = request.params;
   const { title } = request.body;
@@ -67,6 +86,10 @@ app.put('/projects/:id', checkProjectExists, (request, response) => {
   return response.json(projects);
 });
 
+/**
+ * Route params: id
+ * Deleta um projeto com o id informado no parâmetro da rota
+ */
 app.delete('/projects/:id', checkProjectExists, (request, response) => {
   const { id } = request.params;
   const index = getIndexByProject(id);
@@ -76,6 +99,11 @@ app.delete('/projects/:id', checkProjectExists, (request, response) => {
   return response.json(projects);
 });
 
+/**
+ * Route params: id
+ * Request body: title
+ * Adiciona uma nova tarefa no projeto com o id informado no parâmetro da rota
+ */
 app.post('/projects/:id/tasks', checkProjectExists, (request, response) => {
   const { id } = request.params;
   const { title } = request.body;
